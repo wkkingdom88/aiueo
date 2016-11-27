4a890049297892a301c83588ea4a8c3b8610417a
wkkingdom88 <kingdom_hearts_8888@yahoo.co.jp>
2016-11-27 20:59:38 +0900

    Project's Initial Commit

---
+++ www/components/monaca-winjs/winjs-loader.js
@@ -0,0 +1,11 @@
+﻿(function() {
+    if (!window.WinJS || !window.WinJS.UI) {
+        var scriptElemWinJS = document.createElement("script");
+        scriptElemWinJS.src = "//Microsoft.WinJS.1.0/js/base.js";
+        document.head.appendChild(scriptElemWinJS);
+
+        var scriptElemWinJSUI = document.createElement("script");
+        scriptElemWinJSUI.src = "//Microsoft.WinJS.1.0/js/ui.js";
+        document.head.appendChild(scriptElemWinJSUI);
+    }
+})();
